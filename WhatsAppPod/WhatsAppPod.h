27c353420f0f58485908c42a04eaef76017292df
AloneMonkey <liupeiqing1993@163.com>
2017-09-19 19:44:06 +0800

    first commit

---
+++ WhatsAppPod/WhatsAppPod.h
@@ -0,0 +1,19 @@
+//
+//  WhatsAppPod.h
+//  WhatsAppPod
+//
+//  Created by AloneMonkey on 2017/9/19.
+//  Copyright © 2017年 AloneMonkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhatsAppPod.
+FOUNDATION_EXPORT double WhatsAppPodVersionNumber;
+
+//! Project version string for WhatsAppPod.
+FOUNDATION_EXPORT const unsigned char WhatsAppPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatsAppPod/PublicHeader.h>
+
+
